e6180a81630e7b2dd34abc1f0545aa02d92c0912
James <james@jaddie.co.uk>
2012-01-24 05:12:49 +0000

    Removed testing account code from the login command

--- LoginCommand.cs
+++ LoginCommand.cs
@@ -22,26 +22,7 @@ namespace WCellUtilityBot
         }
         public override void Process(WCell.Util.Commands.CmdTrigger<IrcCmdArgs> trigger)
         {
-            var sessionFactory = DBHandler.DBHandler.CreateSessionFactory();
-            using(var session = sessionFactory.OpenSession())
-            {
-                using(var transaction = session.BeginTransaction())
-                {
-                    var acc = new Account {Level = AccountLevel.SuperAdmin, PartylineUsername = "jaddie", QUsername = "jaddie" };
-                    session.SaveOrUpdate(acc);
-                    transaction.Commit();
-                }
-                using(session.BeginTransaction())
-                {
-                    var accounts = session.CreateCriteria(typeof (Account)).List();
-                    foreach (var account in accounts)
-                    {
-                        Console.WriteLine(account);
-                        var acc = (Account) account;
-                        trigger.Reply("Id: " + acc.Id + " AccountLevel: " + acc.Level + " PartylineUsername: " + acc.PartylineUsername + " QUsername: " + acc.QUsername);
-                    }
-                }
-            }
+            //TODO: Write code to login to the bot
         }
     }
 }
